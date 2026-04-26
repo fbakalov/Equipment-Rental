0a985bd0c8f76e62845866a7e09d59118c529d1b
Малин Немов <nilam@teacher.bg>
2026-04-26 10:11:44 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Equipment_Rental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
